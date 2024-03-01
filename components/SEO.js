d64ef4c500e960afb93131f0aa130c65b14c0734
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-03-01 11:11:26 +0700

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 
-export default function SEO({ title, description }) {
+export default function SEO({ title, description, href }) {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <link rel="canonical" href={href} />
     </Head>
   );
 }
